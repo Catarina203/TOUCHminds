5bea090dac48523a221888862e70a30c4a44afdc
Catarina203 <catarinaamonteiro@ua.pt>
2025-08-01 17:26:43 +0100

    Favicon

--- src/components/modulo.js
+++ src/components/modulo.js
@@ -166,7 +166,7 @@ const Modulos = () => {
             alignItems: "center",
           }}
         >
-           <Modal.Title style={{fontWeight: "600", margin: "0", padding: '1rem 6.5rem',}}>
+           <Modal.Title style={{fontWeight: "600", margin: "0", padding: '1rem 7rem',}}>
            ⚡ Energia TOUCH
           </Modal.Title>
         </Modal.Header>
