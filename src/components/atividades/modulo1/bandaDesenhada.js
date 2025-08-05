30a02cb3ad66417bd112be4de9ffe8a2224709fe
Catarina203 <catarinaamonteiro@ua.pt>
2025-08-05 09:27:03 +0100

    alteração da instrução da 1º atividade final

--- src/components/atividades/modulo1/bandaDesenhada.js
+++ src/components/atividades/modulo1/bandaDesenhada.js
@@ -77,17 +77,20 @@ const BandaDesenhada = () => {
 
             {/* INTRODUÇÃO */}
             {pagina === 0 && (
-              <div className="text-center">
+              <div className="text-start py-4 ps-2">
                 <h2 className="fw-bold mb-4" style={{ color: "#234970" }}>{atividade?.titulo || "Ansiedade: Aliada ou Empecilho?"}</h2>
                 <div className="row ">
                   <div className="col-md-12">
                     <p className="lead mb-3">
                       <strong>Sê muito bem-vindo/a à Banda Desenhada da Ansiedade: Aliada ou Empecilho?!</strong><br></br><br></br>
                       <strong>Nesta banda desenhada</strong>, vais conhecer dois tipos de ansiedade:<br></br><br></br>
-                  <strong><strong>Ansiedade comum</strong></strong>, que nos ajuda a enfrentar desafios<br></br>
-                   <strong><strong>Ansiedade SOS</strong></strong>, que pode atrapalhar o nosso bem-estar<br></br><br></br>
-                    </p>
+                  <strong><strong>Ansiedade comum</strong></strong>, que nos ajuda a enfrentar desafios;<br></br>
+                   <strong><strong>Ansiedade SOS</strong></strong>, que pode atrapalhar o nosso bem-estar.<br></br><br></br>
 
+                    <p className="lead mb-3">O teu <strong>desafio</strong> é compreender a diferença entre estas duas formas de ansiedade. <br>
+                    </br>Para isso, lê com atenção os quadros da banda desenhada que se seguem. </p>
+                    </p>
+                    
                     <div className="text-center">
                       <button
                         className="custom-btn-turquoise mt-3 px-4 py-2"
