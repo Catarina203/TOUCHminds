03a2731307b1aedafd51582409b85e21a8c3a10f
Catarina203 <catarinaamonteiro@ua.pt>
2025-08-06 18:08:43 +0100

    final

--- src/components/atividades/modulo1/linhaAnsiedade.js
+++ src/components/atividades/modulo1/linhaAnsiedade.js
@@ -238,8 +238,9 @@ const LinhaAnsiedade = () => {
                          onEnded={() => setVideoStatus(prev => ({ ...prev, antes: true }))}
                           >
                          <source src="/videos/modulo1/linha/linhanormativaantes.mp4" type="video/mp4" />
-                         </video>
-                         </div>
+                          O teu navegador não suporta o elemento de vídeo.
+                            </video>
+                            </div>
                          )}
                     
                     {mostrarAnsiedadeNormativa.durante && (
@@ -250,8 +251,9 @@ const LinhaAnsiedade = () => {
                        onEnded={() => setVideoStatus(prev => ({ ...prev, durante: true }))}
                     >
                       <source src="/videos/modulo1/linha/linhanormativadurante.mp4" type="video/mp4" />
-                     </video>
-                     </div>
+                      O teu navegador não suporta o elemento de vídeo.
+                            </video>
+                            </div>
                      )}
                     
                     {mostrarAnsiedadeNormativa.depois && (
@@ -262,8 +264,9 @@ const LinhaAnsiedade = () => {
                       onEnded={() => setVideoStatus(prev => ({ ...prev, depois: true }))}
                     >
                    <source src="/videos/modulo1/linha/linhanormativadepois.mp4" type="video/mp4" />
-                    </video>
-                   </div>
+                     O teu navegador não suporta o elemento de vídeo.
+                            </video>
+                            </div>
                  )}
 
                     {mostrarAnsiedadeNormativa.conclusao && (
@@ -274,6 +277,7 @@ const LinhaAnsiedade = () => {
                      onEnded={() => setVideoStatus(prev => ({ ...prev, conclusao: true }))}
                    >
                    <source src="/videos/modulo1/linha/linhanormativaconclusao.mp4" type="video/mp4" />
+                   O teu navegador não suporta o elemento de vídeo.
                    </video>
                    </div>
                    )}    
@@ -366,6 +370,7 @@ const LinhaAnsiedade = () => {
                        onEnded={() => setVideoStatusPatologica(prev => ({ ...prev, antes: true }))}
                         >
                         <source src="/videos/modulo1/linha/linhapatologicaantes.mp4" type="video/mp4" />
+                        O teu navegador não suporta o elemento de vídeo.
                        </video>
                        </div>
                        )}
@@ -378,6 +383,7 @@ const LinhaAnsiedade = () => {
                     onEnded={() => setVideoStatusPatologica(prev => ({ ...prev, durante: true }))}
                      >
                      <source src="/videos/modulo1/linha/linhapatologicadurante.mp4" type="video/mp4" />
+                     O teu navegador não suporta o elemento de vídeo.
                     </video>
                     </div>
                     )}
@@ -390,6 +396,7 @@ const LinhaAnsiedade = () => {
                     onEnded={() => setVideoStatusPatologica(prev => ({ ...prev, depois: true }))}
                    >
                    <source src="/videos/modulo1/linha/linhapatologicadepois.mp4" type="video/mp4" />
+                   O teu navegador não suporta o elemento de vídeo.
                      </video>
                     </div>
                     )}
@@ -402,6 +409,7 @@ const LinhaAnsiedade = () => {
                 onEnded={() => setVideoStatusPatologica(prev => ({ ...prev, conclusao: true }))}
                >
                 <source src="/videos/modulo1/linha/linhapatologicaconclusao.mp4" type="video/mp4" />
+                O teu navegador não suporta o elemento de vídeo.
                 </video>
               </div>
                 )}
@@ -423,19 +431,27 @@ const LinhaAnsiedade = () => {
 
                 </>
               )}
-              {pagina === 4 && (
+                      {pagina === 4 && (
                 <>
                   {/* CONCLUSÃO DA ATIVIDADE */}
-                    <div className="text-center"></div>
-                    <h4 className="text-center fw-bold" style={{ color: "#234970" }}>Conclusão da Atividade</h4>
-                    <p className="lead">
-                      <strong>Espero que esta atividade tenha sido um primeiro passo importante para compreenderes melhor as diferentes intensidades da ansiedade! </strong> <br></br><br></br>
-                      <strong>Lembra-te</strong> que a <strong>ansiedade</strong> é útil e ajuda-nos a <strong>preparar</strong> para situações difíceis. <br></br><br></br>
-                      No entanto, alguns <strong>comportamentos</strong>, como <strong>evitar</strong> certas situações, podem acabar por nos <strong>manter ansiosos</strong>, de tal maneira que parece que toda a nossa 
-                      vida gira em torno da ansiedade. <br></br><br></br>
-                      <strong>Nem todo o tipo de evitamento</strong> é problemático; às vezes, pode ser a única maneira de lidar com uma situação difícil.<br></br><br></br>
-                       O importante é <strong>reconheceres</strong> quando o <strong>evitamento</strong> ajuda e quando ele se torna um <strong>problema</strong>. <br></br><br></br>
-                    </p>
+                  <div className="text-center">
+                    <h4 className="fw-bold" style={{ color: "#234970" }}>
+                      Conclusão da Atividade
+                    </h4>
+                  </div>
+
+                  <p className="lead">
+                    Espero que esta atividade tenha ajudado a perceber como <strong>os nossos pensamentos, emoções e comportamentos</strong> influenciam a forma como enfrentamos uma situação desafiadora! 
+                    <br /><br />
+                    Lembra-te que a ansiedade é útil e ajuda-nos a <strong>preparar</strong> para situações difíceis. 
+                    <br /><br />
+                    No entanto, alguns <strong>comportamentos</strong>, como <strong>evitar</strong> certas situações, podem acabar por nos <strong>manter ansiosos</strong>, de tal maneira que parece que toda a nossa vida gira em torno da ansiedade. 
+                    <br /><br />
+                    <strong>Nem todo o tipo de evitamento</strong> é problemático; às vezes, pode ser a única maneira de lidar com uma situação difícil.
+                    <br /><br />
+                    O importante é <strong>reconheceres</strong> quando o <strong>evitamento</strong> ajuda e quando ele se torna um <strong>problema</strong>. 
+                    <br /><br />
+                  </p>
 
                   <div className="d-flex justify-content-between mt-4">
                     <button className="custom-btn-pink" onClick={retrocederPagina}>
@@ -449,11 +465,11 @@ const LinhaAnsiedade = () => {
                   </div>
                 </>
               )}
-            
-          </div>
-        </div>
-      </div>
-    </div>
+
+          </div> {/* fecha .container p-5 */}
+        </div> {/* fecha .col */}
+      </div> {/* fecha .row */}
+    </div> {/* fecha .container-fluid */}
   );
 };
 
